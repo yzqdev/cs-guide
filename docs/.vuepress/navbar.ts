e12d7b18fa4159b7cdcd124bfb061bdc30bfda9b
yzqdev <yzqdev@outlook.com>
2022-04-02 16:05:17 +0800

    add sql

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,15 +2,31 @@ import { defineNavbarConfig } from "vuepress-theme-hope";
 
 export default defineNavbarConfig([
   // { text: "文章", icon: "home", link: "/article/" },
-  { text: "cg教程", icon: "software", link: "/cg-tutor/" },
+
   { text: "前端教程", icon: "html", link: "/frontend/" },
-  { text: "游戏教程", icon: "game", link: "/game-tutor/" },
 
   { text: "java教程", icon: "java", link: "/java-tutor/" },
+  {
+    text: "git教程",
+    icon: "git",
+    link: "https://yzqdev.github.io/git-tutor",
+  },
   { text: "linux教程", icon: "linux", link: "/linux-tutor/" },
   { text: "python教程", icon: "python", link: "/python-tutor/" },
   { text: "c#教程", icon: "csharp", link: "/csharp-tutor/" },
   { text: "windows教程", icon: "windows", link: "/windows-tutor/" },
+  {
+    text: "cg教程",
+    icon: "game",
+    children: [
+      {
+        text: "cg教程",
+        icon: "software",
+        link: "/cg-tutor/",
+      },
+      { text: "游戏教程", icon: "game", link: "/game-tutor/" },
+    ],
+  },
   {
     text: "其他",
     icon: "note",
@@ -24,11 +40,6 @@ export default defineNavbarConfig([
         text: "linux基础教程",
         icon: "computer",
         link: "https://yzqbooks.github.io/Linux-Tutorial/",
-      },
-      {
-        text: "git教程",
-        icon: "git",
-        link: "https://yzqdev.github.io/git-tutor",
       },
 
       {
--- docs/game-tutor/steam-game.md
+++
@@ -1,9 +0,0 @@
-# steam game download
-
-《死或生5》
-steam://install/311730
-解锁dlc办法::
-DLC入库代码：
-steam://install/531157
-死或生6本体入库代码：[https://bbs.3dmgame.com/thread-6031238-1-2.html](https://bbs.3dmgame.com/thread-6031238-1-2.html)
-steam://install/838380
