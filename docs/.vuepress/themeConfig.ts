f41a55d2423a1abd59b7556e16dd0e878b4093fc
yzqdev <yzqdev@outlook.com>
2022-05-17 06:43:38 +0800

    update pwa

--- docs/.vuepress/themeConfig.ts
+++ docs/.vuepress/themeConfig.ts
@@ -79,12 +79,38 @@ export default hopeTheme({
       themeColor: "#46bd87",
       appendBase: true,
       apple: {
-        icon: "/images/cs-guide.webp",
+        icon: "/assets/icon/apple-icon-152.png",
         statusBarColor: "black",
       },
       msTile: {
-        image: "/images/cs-guide.webp",
+        image: "/assets/icon/ms-icon-144.png",
         color: "#ffffff",
+      },
+      manifest: {
+        icons: [
+          {
+            src: "/assets/icon/chrome-mask-512.png",
+            sizes: "512x512",
+            purpose: "maskable",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-mask-192.png",
+            sizes: "192x192",
+            purpose: "maskable",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
       },
     },
     // 你也可以使用 Waline
