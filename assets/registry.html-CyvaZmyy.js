3657a4cb990e97c84dea02d1a7821c74580d0946
yzqdev <yzqdev@outlook.com>
2025-02-20 12:51:44 +0000

    Deploying to gh-pages from @ yzqdev/cs-guide@fb6e252c6e554e6038a32a2cb3b987f4406465ab 🚀

---
+++ assets/registry.html-CyvaZmyy.js
@@ -0,0 +1,15 @@
+import{_ as s,c as t,a as n,o as a}from"./app-Die-Lcms.js";const r={};function p(i,e){return a(),t("div",null,e[0]||(e[0]=[n(`<h1 id="镜像源使用" tabindex="-1"><a class="header-anchor" href="#镜像源使用"><span>镜像源使用</span></a></h1><p>bootcdn</p><p>unpkg</p><p>jsdelivr</p><p>npmmirror</p><div class="language-bash" data-highlighter="prismjs" data-ext="sh" data-title="sh"><pre><code><span class="line"><span class="token comment"># 获取目录信息 /\${pkg}/\${versionOrTag}/files?meta</span></span>
+<span class="line">https://registry.npmmirror.com/antd/5.5.2/files?meta</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># 获取文件内容 /\${pkg}/\${versionOrTag}/files/\${path}</span></span>
+<span class="line">https://registry.npmmirror.com/antd/5.5.0/files/lib/index.js</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># 获取文件元信息 /\${pkg}/\${versionOrTag}/files/\${path}?meta</span></span>
+<span class="line">https://registry.npmmirror.com/antd/5.5.0/files/lib/index.js?meta</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># 获取入口文件内容 /\${pkg}/\${versionOrTag}/files</span></span>
+<span class="line">https://registry.npmmirror.com/antd/latest/files</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># 支持 Semver Range</span></span>
+<span class="line">https://registry.npmmirror.com/antd/^5/files/lib/index.js</span>
+<span class="line"></span></code></pre></div>`,6)]))}const l=s(r,[["render",p],["__file","registry.html.vue"]]),c=JSON.parse('{"path":"/cs-tips/frontend/framework/registry.html","title":"镜像源使用","lang":"zh-CN","frontmatter":{"description":"镜像源使用 bootcdn unpkg jsdelivr npmmirror","head":[["meta",{"property":"og:url","content":"https://yzqdev.github.io/cs-guide/cs-guide/cs-tips/frontend/framework/registry.html"}],["meta",{"property":"og:site_name","content":"cs-guide"}],["meta",{"property":"og:title","content":"镜像源使用"}],["meta",{"property":"og:description","content":"镜像源使用 bootcdn unpkg jsdelivr npmmirror"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-04-26T05:15:13.000Z"}],["meta",{"property":"article:modified_time","content":"2024-04-26T05:15:13.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"镜像源使用\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-04-26T05:15:13.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"yzqdev\\",\\"url\\":\\"http://www.yzqdev.top\\"}]}"]]},"headers":[],"git":{"createdTime":1714108513000,"updatedTime":1714108513000,"contributors":[{"name":"yzqdev","username":"yzqdev","email":"yzqdev@outlook.com","commits":1,"url":"https://github.com/yzqdev"}]},"readingTime":{"minutes":0.26,"words":78},"filePathRelative":"cs-tips/frontend/framework/registry.md","localizedDate":"2024年4月26日","autoDesc":true}');export{l as comp,c as data};
